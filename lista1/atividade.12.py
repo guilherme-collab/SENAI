89bd0c6b7c68e417a99b59a95684c6ebf8d3a0c2
guilherme-collab <guilherme_oliveira41@estudante.sesisenai.org.br>
2025-05-14 12:17:44 +0000

    update

--- lista1/atividade.12.py
+++ lista1/atividade.12.py
@@ -1,4 +1,9 @@
-primeiro = float(input("Digite o primeiro numero: "))
-segundo = float(input("Digite o segundo numero: "))
-total = primeiro / segundo
-print(f"A divisão dessses numeros é de {total}")
+soma = 0
+n = 3
+for i in range(1, n + 1):
+    numero = float(input(f"Digite o {i}º número: "))
+    soma += numero
+
+multiplicacao = soma 
+
+print("A média dos números digitados é: ", multiplicacao)
