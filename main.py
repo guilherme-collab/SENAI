7d60096a7e5b9387382db36a53aad08adaae8f88
guilherme-collab <guilherme_oliveira41@estudante.sesisenai.org.br>
2025-05-14 13:54:44 +0000

    update

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import os
 
 #define a pasta onde estão os arquivos HTML 
 # (neste caso, a raiz do produto)
-template_dir = os.path.abspath(os.path.dirname(_file_))
+template_dir = os.path.abspath(os.path.dirname(__file__))
 app = flask(__name__, template_folder=template_dir)
 
+#Direcionamento para a página index.html
+@app.route("/")
+def nome ():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host= "0.0.0.0", port=3000)
